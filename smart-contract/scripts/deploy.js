437bfb928b8fe8ea1e4ef65b32214d2a66e87eda
Ayan Masood <ayan.m.dev@gmail.com>
2023-02-26 10:53:55 +0530

    Deploy

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,31 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+ const StackUp = await hre.ethers.getContractFactory("StackUp");
+ const stackUp = await StackUp.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+ await stackUp.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+ console.log("stackUp deployed to:", stackUp.address);
 
-  await lock.deployed();
+ let adminAddr = await stackUp.admin();
+ console.log("admin address:", adminAddr);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+ await stackUp.createQuest("Introduction to Hardhat", 2, 600);
+ await stackUp.createQuest("Unit Testing with Hardhat", 4, 500);
+ await stackUp.createQuest("Debugging and Deploying with Hardhat", 5, 400);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+main()
+ .then(() => process.exit(0))
+ .catch((error) => {
   console.error(error);
-  process.exitCode = 1;
-});
+  process.exit(1);
+ });
